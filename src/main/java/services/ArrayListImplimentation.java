cf5cff3030163e6bd2c1687182f5f0143ee4f676
UK <usman.khalid@hotelkeyapp.com>
2022-11-10 15:08:42 +0500

    Restoring commit

---
+++ src/main/java/services/ArrayListImplimentation.java
@@ -0,0 +1,12 @@
+package services;
+import java.util.ArrayList;
+
+public class ArrayListImplimentation {
+    public static void main(String[] args) {
+        ArrayList<String> Student = new ArrayList<String>();
+        Student.add("Usman");
+        Student.add("Khalid");
+        Student.add("Ali");
+        Student.add("Umer");
+    }
+}
