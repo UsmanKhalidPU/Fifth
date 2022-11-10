6c4977147e1acfbed7011ef38ce84e2ff33951b5
UK <usman.khalid@hotelkeyapp.com>
2022-11-10 15:47:27 +0500

    Linked List Implimentation

---
+++ src/main/java/services/LLImplimentation.java
@@ -0,0 +1,28 @@
+package services;
+import java.util.LinkedList;
+
+public class LLImplimentation {
+    public static void main(String[] args) {
+        LinkedList<String> Student = new LinkedList<String>();
+        Student.add("Usman");
+        Student.add("Khalid");
+        Student.add("Ali");
+        Student.add("Umer");
+
+        System.out.println(Student.get(2));
+        Student.set(2,"Shahab");
+        System.out.println(Student.get(2));
+
+        System.out.println(Student.size());
+        Student.remove(2);
+        System.out.println(Student.size());
+        //System.out.println(Student.get(2));
+        Student.clear();
+        System.out.println(Student.size());
+
+        Student.addFirst("Sara");
+        System.out.println(Student.get(0));
+        System.out.println(Student.size());
+    }
+
+}
