1898ea46e99533cb2766289dfdd01754f58ab6ef
UK <usman.khalid@hotelkeyapp.com>
2022-11-10 16:48:52 +0500

    Hash Map Implimentation

---
+++ src/main/java/services/HMImplimentation.java
@@ -0,0 +1,21 @@
+package services;
+import java.util.HashMap;
+
+public class HMImplimentation {
+    public static void main(String[] args) {
+        HashMap<String, String> StudentCities = new HashMap<String, String>();
+
+        // Add keys and values (Country, City)
+        StudentCities.put("Usman", "Lahore");
+        StudentCities.put("Khalid", "Bahawalpur");
+        StudentCities.put("Ali", "Kasur");
+        StudentCities.put("Umer", "Islamabad");
+        System.out.println(StudentCities);
+
+        System.out.println(StudentCities.get("Usman"));
+        System.out.println(StudentCities.size());
+        StudentCities.remove("Usman");
+        System.out.println(StudentCities.size());
+
+    }
+}
