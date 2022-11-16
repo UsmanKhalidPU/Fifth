ade128e8a65357947965ee796ec349971ca17bbb
UK <usman.khalid@hotelkeyapp.com>
2022-11-16 15:16:10 +0500

    Json Object created

---
+++ src/main/java/services/ApiImplimentation.java
@@ -0,0 +1,17 @@
+package services;
+
+import org.json.JSONObject;
+
+public class ApiImplimentation {
+    public static void main(String[] args) {
+
+        JSONObject obj = new JSONObject();
+
+        obj.put("name", "foo");
+        obj.put("num", new Integer(100));
+        obj.put("balance", new Double(1000.21));
+        obj.put("is_vip", new Boolean(true));
+
+        System.out.print(obj);
+    }
+}
